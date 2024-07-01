558f16eab3be231c8af3682af155a942d063e8b3
jCabala <79060739+jCabala@users.noreply.github.com>
2024-07-01 09:43:31 +0200

    Time: 1604 ms (46.70%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,18 +1,8 @@
-SELECT person_name
-FROM Queue Q
-WHERE (
-    SELECT SUM(weight)
-    FROM Queue
-    WHERE turn <= Q.turn 
-) <= 1000
-AND ((
-        SELECT SUM(weight)
-        FROM Queue
-        WHERE turn <= Q.turn + 1
-    ) > 1000 
-    OR
-        turn IN (
-            SELECT COUNT(person_id)
-            FROM Queue
-    )
-)
+SELECT Q1.person_name
+FROM Queue Q1
+JOIN Queue Q2
+ON Q1.turn >= Q2.turn
+GROUP BY Q1.turn
+HAVING SUM(Q2.weight) <= 1000
+ORDER BY SUM(Q2.weight) DESC
+LIMIT 1
