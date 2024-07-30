6278ad8b6cb300c510be1cde119f9349930407d1
jCabala <79060739+jCabala@users.noreply.github.com>
2024-07-30 22:58:48 +0200

    Time: 0 ms (100.00%) | Memory: 7.8 MB (9.98%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        ios::sync_with_stdio(0); cin.tie(0);
         int sptr = 0;
 
         for(int i = 0; i < t.length(); i++) {
