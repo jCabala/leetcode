0a52ea397de979d4b68525fa0db97bb9e9a40c58
jCabala <79060739+jCabala@users.noreply.github.com>
2024-08-02 21:06:03 +0200

    Time: 3 ms (57.67%) | Memory: 11.7 MB (6.02%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int negs[1005], poss[1005];
+        vector<int> occ;
+
+        for(int i = 0; i <= 1000; i++) negs[i] = poss[i] = 0;
+
+        for (int u: arr) {
+            if (u >= 0) {
+                poss[u]++;
+            } else {
+                negs[-1 * u]++;
+            }
+        }
+
+        for (int i = 0; i <= 1000; i++) {
+            if (poss[i] != 0) {
+                occ.push_back(poss[i]);
+            }
+
+            if (negs[i] != 0) {
+                occ.push_back(negs[i]);
+            }
+        }
+        
+        sort(occ.begin(), occ.end());
+        for(int i = 0; i < occ.size() - 1; i++) {
+            if (occ[i] == occ[i + 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
