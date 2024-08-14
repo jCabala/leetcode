c14c5427448f23c1c475ca9c5fb3aed488d8f2e6
jCabala <79060739+jCabala@users.noreply.github.com>
2024-08-14 23:48:25 +0200

    Time: 0 ms (100.00%) | Memory: 14.2 MB (23.04%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        ios::sync_with_stdio(0); cin.tie(0);
+        vector<bool> visited(rooms.size(), false);
+        for(auto u: visited) cout << u;
+        dfs(0, rooms, visited);
+
+        bool res = true;
+        for(auto u: visited) res &= u;
+        return res;
+    }
+
+    void dfs(int node, vector<vector<int>>& rooms, vector<bool>& visited) {
+        if(visited[node]) return;
+
+        visited[node] = true;
+        for(auto u: rooms[node]) dfs(u, rooms, visited);
+    }
+};
