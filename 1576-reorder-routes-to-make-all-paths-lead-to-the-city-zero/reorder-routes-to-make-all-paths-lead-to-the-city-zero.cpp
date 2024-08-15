dc2ecfc1b0b3831deb92e799aa05da27fdd239b7
jCabala <79060739+jCabala@users.noreply.github.com>
2024-08-15 10:25:42 +0200

    Time: 579 ms (15.38%) | Memory: 176 MB (6.52%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+int ans;
+    int minReorder(int n, vector<vector<int>>& connections) {
+        ans = 0;
+        vector<vector<vector<int>>> graph(n, vector<vector<int>>());
+
+        for(auto u: connections) {
+           graph[u[0]].push_back(u);
+           graph[u[1]].push_back(u);
+        }
+
+        dfs(0, -1, graph);
+        return ans;
+    }
+
+    void dfs(int node, int parent, vector<vector<vector<int>>>& graph) {
+        for(auto edge: graph[node]) {
+            int a = edge[1];
+            int b = edge[0];
+
+            if(a == parent || b == parent) continue;
+            if(b == node) {
+                swap(a, b);
+                ans++;
+            }
+
+            dfs(b, node, graph);
+        }
+    }
+};
