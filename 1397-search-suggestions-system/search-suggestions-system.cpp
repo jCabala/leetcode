0aa3b673653ec6da727422c802b1165bb0eefd62
jCabala <79060739+jCabala@users.noreply.github.com>
2024-08-29 10:56:17 +0200

    Time: 22 ms (95.68%) | Memory: 30.1 MB (85.46%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(), products.end());
+        vector<vector<string>> ans;
+        int left = 0, right = products.size() - 1;
+
+        for (int i = 0; i < searchWord.length(); i++) {
+            char c = searchWord[i];
+            vector<string> res;
+
+            while (left <= right && (products[left].size() <= i || products[left][i] < c)) left++;
+            while (left <= right && (products[right].size() <= i || products[right][i] > c)) right--;
+
+            for (int j = 0; j < 3 && left + j <= right; j++) {
+                res.push_back(products[left + j]);
+            }
+            ans.push_back(res);
+        }
+
+        return ans;
+    }
+};
