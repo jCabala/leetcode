4d10616298388fb8f55c354ede4484a9ec4a0717
jCabala <79060739+jCabala@users.noreply.github.com>
2024-09-20 21:40:59 +0200

    Time: 6 ms (7.08%) | Memory: 11.5 MB (23.50%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int it = 0;
+        ios::sync_with_stdio(0); cin.tie(0);
         vector<int> ans;
         int it1 = 0;
         int it2 = 0;
