6ab6452902d9c1e26ea9ab42f88dc26fcf9a0389
jCabala <79060739+jCabala@users.noreply.github.com>
2024-10-15 13:50:58 +0100

    Time: 36 ms (98.58%) | Memory: 62.1 MB (27.36%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int maxArea(vector<int>& h) {
         ios::sync_with_stdio(0); cin.tie(0);
-        int rhs = h.size() - 1;
-        int lhs = 0;
-        int res = 0;
+        int beg = 0, end = h.size() - 1;
+        int ans = (end - beg) * min(h[beg], h[end]);
 
-        while(rhs != lhs) {
-            res = max(res, (rhs - lhs) * min(h[lhs], h[rhs]));
-
-            if (h[lhs] <= h[rhs]) {
-                lhs++;
+        while(beg < end) {
+            ans = max(ans, (end - beg) * min(h[beg], h[end]));
+            if (h[beg] > h[end]) {
+                end--;
             } else {
-                rhs--;
+                beg++;
             }
         }
 
-        return res;
+        return ans;
     }
 };
-
-// maximize min(h[i], h[j]) * |i - j|
