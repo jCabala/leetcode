ce6b6bd2fef1c0b1a2cb707557a7ffa957d45e4b
jCabala <79060739+jCabala@users.noreply.github.com>
2025-11-11 10:22:10 +0000

    Time: 0 ms (100.00%) | Memory: 12.6 MB (12.91%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        value = [0 for _ in range(n + 1)] 
+        value[0] = 1
+        value[1] = 2
+
+        for i in range(2, n):
+            value[i] = value[i-1] + value[i-2]
+
+
+        return value[n-1]
+
+        
